e235d1ce4697a491bc3316062f90d22dea065869
Ollie Cross <iio.olivercross@gmail.com>
2023-02-05 17:16:58 +0000

    Basic form layout and event handlers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import { v4 } from "uuid";
+//Components
+import { useState } from "react";
+import Form from "./components/Form";
+//Styles
 import "./main.scss";
 
 function App() {
+  const [newBookmark, setNewBookmark] = useState({});
+
+  const handleChange = ({ target }) => {
+    setNewBookmark({ ...newBookmark, [target.name]: target.value, id: v4() });
+  };
+
+  const validUrl = (url) => {
+    try {
+      if (new URL(url)) {
+        return true;
+      }
+    } catch (e) {
+      return false;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(newBookmark);
+  };
   return (
     <main>
       <h1>bookmarkd</h1>
+      <Form handleChange={handleChange} handleSubmit={handleSubmit} />
     </main>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,31 @@
-const Form = () => {
-  return <div>Form</div>;
+const Form = ({ handleChange, handleSubmit }) => {
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      <div className="form__section">
+        <label htmlFor="name" className="form__label">
+          title
+        </label>
+        <input
+          type="text"
+          className="form__input"
+          name="title"
+          onChange={handleChange}
+        />
+      </div>
+      <div className="form__section">
+        <label htmlFor="url" className="form__label">
+          URL
+        </label>
+        <input
+          type="text"
+          className="form__input"
+          name="url"
+          onChange={handleChange}
+        />
+      </div>
+      <button className="form__cta">Save!</button>
+    </form>
+  );
 };
 
 export default Form;
